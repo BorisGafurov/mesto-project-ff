7511d2d9bc79b9f142ada958eb99f86e7dafe960
BGmack <gafur08@yandex.ru>
2024-02-09 23:04:00 +0400

    1.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,47 @@
+
 // @todo: Темплейт карточки
-
+    
+    
 // @todo: DOM узлы
-
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardContainer = document.querySelector('.places__list');
+    const addButton = document.querySelector('.profile__add-button');
 // @todo: Функция создания карточки
+     function addCard(create, deleteCallback) {
+  
+  const clonTemplate = cardTemplate.cloneNode(true).querySelector('.card');
 
+  
+  const cardImage = clonTemplate.querySelector('.card__image');
+  cardImage.src = create.link;
+  cardImage.alt = create.name;
+
+  const cardTitle = clonTemplate.querySelector('.card__title');
+  cardTitle.textContent = create.name;
+
+ 
+  const deleteButton = clonTemplate.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {
+    deleteCallback(clonTemplate);
+  });
+
+  return clonTemplate;
+}
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(function(cardData) {
+  const cardElement = addCard(cardData, function(card) {
+    
+    card.remove();
+  });
+
+  placesList.append(cardElement);
+});
+
+
+   
+
+
