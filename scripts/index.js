4c3c6bd4da8e8e4330c2864d8def6d56f57785fc
BGmack <gafur08@yandex.ru>
2024-02-10 21:56:14 +0400

    2.2

--- scripts/index.js
+++ scripts/index.js
@@ -7,39 +7,31 @@
     const cardContainer = document.querySelector('.places__list');
     const addButton = document.querySelector('.profile__add-button');
 // @todo: Функция создания карточки
-     function addCard(create, deleteCallback) {
-  
-  const clonTemplate = cardTemplate.cloneNode(true).querySelector('.card');
+     function createCard(cardData, deleteCallback) { 
+    const clonTemplate = cardTemplate.cloneNode(true).querySelector('.card');
+    const cardImage = clonTemplate.querySelector('.card__image');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    
+    const cardTitle = clonTemplate.querySelector('.card__title');
+    cardTitle.textContent = cardData.name;
 
-  
-  const cardImage = clonTemplate.querySelector('.card__image');
-  cardImage.src = create.link;
-  cardImage.alt = create.name;
-
-  const cardTitle = clonTemplate.querySelector('.card__title');
-  cardTitle.textContent = create.name;
-
- 
-  const deleteButton = clonTemplate.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', function() {
+    const deleteButton = clonTemplate.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function() {
     deleteCallback(clonTemplate);
-  });
+    });
 
-  return clonTemplate;
+    return clonTemplate;
 }
 // @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-const placesList = document.querySelector('.places__list');
-
-initialCards.forEach(function(cardData) {
-  const cardElement = addCard(cardData, function(card) {
-    
+    function deleteCard(card) {
     card.remove();
-  });
-
-  placesList.append(cardElement);
-});
+    }
+// @todo: Вывести карточки на страницу
+    initialCards.forEach(function(cardData) {
+    const cardElement = createCard(cardData, deleteCard) 
+    cardContainer.append(cardElement);
+    });
 
 
    
